3ce9ec00b1147fcab2b6de474b2a27c22f738621
CoreVanta <eng.freethinker@gmail.com>
2025-12-27 07:06:09 +0200

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Replace with your Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCGRAz4hNy9737rRC2XhGjihrroVFrBnyE",
+	authDomain: "doctors-89de9.firebaseapp.com",
+	projectId: "doctors-89de9",
+	storageBucket: "doctors-89de9.firebasestorage.app",
+	messagingSenderId: "843277629462",
+	appId: "1:843277629462:web:c6ef4918f469d13dff5d37",
+	measurementId: "G-RVBRHCX77H"
 };
 
 const app = initializeApp(firebaseConfig);
